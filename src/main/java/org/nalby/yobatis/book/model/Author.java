89ee1290263f737130f3f398b74c7488f0423abe
Kyle Lin <linbaiye@gmail.com>
2018-02-04 14:57:27 +0800

    fix config.

--- src/main/java/org/nalby/yobatis/book/model/Author.java
+++
@@ -1,6 +0,0 @@
-package org.nalby.yobatis.book.model;
-
-import org.nalby.yobatis.book.model.base.BaseAuthor;
-
-public class Author extends BaseAuthor {
-}
--- src/main/java/org/nalby/yobatis/book/model/Book.java
+++
@@ -1,6 +0,0 @@
-package org.nalby.yobatis.book.model;
-
-import org.nalby.yobatis.book.model.base.BaseBook;
-
-public class Book extends BaseBook {
-}
