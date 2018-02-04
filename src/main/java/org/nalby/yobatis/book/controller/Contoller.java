bc5746712a72457bcbd07d9d8bee79891b2ca593
Kyle Lin <linbaiye@gmail.com>
2018-02-04 15:05:50 +0800

    fix controller.

--- src/main/java/org/nalby/yobatis/book/controller/Contoller.java
+++ src/main/java/org/nalby/yobatis/book/controller/Contoller.java
@@ -22,13 +22,9 @@ public class Contoller {
 	@Autowired
 	private BookService bookService;
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
-	public Response<Book> listBooks() {
-		return Response.ok(bookService.getById(1L));
-	}
-	
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = "application/json")
 	public Response<Book> getByName(@PathVariable(value = "name") String name) {
+		logger.info("Getting book by name:{}.", name);
 		return Response.ok(bookService.getByName(name));
 	}
 	
@@ -36,7 +32,6 @@ public class Contoller {
 	public Response<List<Book>> getByNameOrAuthor(
 			@PathVariable(value = "bookName") String bookName,
 			@PathVariable(value = "authorName") String authorName) {
-		logger.info("name:{}, name:{}, name:{}, name:{}, name:{}.", bookName, bookName, authorName, authorName, "test");
 		return Response.ok(bookService.nameEqualOrAuthorIs(bookName, authorName));
 	}
 }
